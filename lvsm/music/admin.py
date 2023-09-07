f2bc0fe8f7b693bafb7573ca923ac28d6a752e16
lavosami <vap210504@gmail.com>
2023-09-07 13:36:23 +0300

    now you can add many photos to your post through thw MusicAdmin

--- lvsm/music/admin.py
+++ lvsm/music/admin.py
@@ -3,6 +3,11 @@ from django.contrib import admin
 from music.models import *
 
 
+class ImageInline(admin.TabularInline):
+    model = Image
+    extra = 1
+
+
 class MusicAdmin(admin.ModelAdmin):
     list_display = ['id', 'title', 'time_create', 'photo', 'is_published']
     list_display_links = ['id', 'title']
@@ -10,6 +15,7 @@ class MusicAdmin(admin.ModelAdmin):
     list_editable = ['is_published']
     list_filter = ['is_published', 'time_create']
     prepopulated_fields = {"slug": ("title",)}
+    inlines = [ImageInline]
 
 
 class GenreAdmin(admin.ModelAdmin):
@@ -19,12 +25,12 @@ class GenreAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("name",)}
 
 
-class ImageAdmin(admin.ModelAdmin):
-    list_display = ('id', 'photo', 'music_id')
-    list_display_links = ('music_id', )
-    search_fields = ('music_id',)
+# class ImageAdmin(admin.ModelAdmin):
+#     list_display = ('id', 'photo', 'music_id')
+#     list_display_links = ('music_id', )
+#     search_fields = ('music_id',)
 
 
 admin.site.register(Music, MusicAdmin)
 admin.site.register(Genre, GenreAdmin)
-admin.site.register(Image, ImageAdmin)
+# admin.site.register(Image, ImageAdmin)
